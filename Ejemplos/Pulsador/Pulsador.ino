4b151a4513fca4555954610fccb82fab2c12bdcd
javacasm <javacasm@gmail.com>
2016-12-05 20:00:05 +0100

    Pulsador

---
+++ Ejemplos/Pulsador/Pulsador.ino
@@ -0,0 +1,23 @@
+/* Kit Bricogeek
+*
+* conexion pulsador
+* 
+* by @javacasm 
+ */ 
+  
+#define pinPulsador 8
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinPulsador,INPUT_PULLUP);
+}
+
+void loop() {
+
+  int valor=digitalRead(pinPulsador);
+  if(valor==LOW){
+    Serial.println("Pulsado!");
+    delay(500);
+  }
+  
+}
